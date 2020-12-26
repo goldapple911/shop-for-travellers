abe139691109a927316ea99f1e11ec18b6be229a
kevin <goldapple820@gmail.com>
2020-12-26 10:29:41 +0100

    mongoDB connection & private info in dev.js

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
+const mongoose = require("mongoose");
+const config = require("./config/key");
+
+mongoose.connect(config.mongoURL, { useNewUrlParser: true, }).then(()=> console.log("MongoDB connected.")).catch(err=>console.log(err))
 
 const port = 5000;
 
 app.get('/', (req, res)=>{
     res.send("Hello world");
 })
+
+
+
 
 
 
