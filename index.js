54fc31be635f6e4a6708e4af12b1b851d5f66b4c
kevin <goldapple820@gmail.com>
2020-12-28 09:05:08 +0100

    register user added

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 const express = require('express');
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+
 const config = require("./config/key");
+const port = 5000;
+
+const {User} = require("./models/user");
 
 mongoose.connect(config.mongoURL, { useNewUrlParser: true, }).then(()=> console.log("MongoDB connected.")).catch(err=>console.log(err))
 
-const port = 5000;
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.post('/api/users/register', (req, res)=>{
+    const user = new User(req.body)
+    user.save((err, userData)=>{
+        if(err){
+            return res.json({success: false, err})
+        }
+        return res.status(200);
+    })
+
+})
+
 
 app.get('/', (req, res)=>{
     res.send("Hello world");
 })
-
-
-
 
 
 
