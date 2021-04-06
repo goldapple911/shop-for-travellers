c35ef570edaaf5f3ec078562070c020394c6fac3
kevin <goldapple820@gmail.com>
2021-04-06 14:29:31 +0200

    readme added

---
+++ README.md
@@ -0,0 +1,19 @@
+# Travel-shop
+
+This Travel shopping web is the e-commercial web site that introduce the travel places.
+The web project is built on the React framework.
+
+## Getting started
+
+```bash
+npm install
+
+cd client
+npm install
+
+npm run dev
+```
+
+## Live Demo
+
+https://travel-shop.herokuapp.com/
--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -28,7 +28,6 @@ function LandingPage(props) {
 
   const getProducts = (variable) => {
     axios.post("/api/product/getProducts", variable).then((response) => {
-      console.log(response);
       if (response.data.success) {
         if (variable.loadMore) {
           setProducts([...products, ...response.data.products]);
