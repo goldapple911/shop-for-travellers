9c7b4ed10b5f4033b905bea7f4333a896351d4ae
kevin <goldapple820@gmail.com>
2021-04-06 13:01:36 +0200

    heroku deploy -v1

--- server/index.js
+++ server/index.js
@@ -26,8 +26,19 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use('/uploads', express.static('uploads'));
 app.use('/api/user', require('./routes/userRoute'));
-app.use('/api/product', require('./routes/product'))
+app.use('/api/product', require('./routes/product'));
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  // All the javascript and css files will be read and served from this folder
+  app.use(express.static('client/build'));
+
+  // index.html for all page routes    html or routing and naviagtion
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+  });
+}
 
 app.listen(port, () => {
   console.log("Server is running on " + port);
