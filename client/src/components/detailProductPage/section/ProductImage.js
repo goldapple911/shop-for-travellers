47aa67661f93c670d10361661c8f04cc1fd6301d
kevin <goldapple820@gmail.com>
2021-04-06 14:14:52 +0200

    heroku deploy -v6

--- client/src/components/detailProductPage/section/ProductImage.js
+++ client/src/components/detailProductPage/section/ProductImage.js
@@ -6,11 +6,17 @@ function ProductImage(props) {
     useEffect(() => {
         if(props.detail.images && props.detail.images.length>0){
             let images =[];
-      
+
+            let url="";
+            if(process.env.NODE_ENV ==="production"){
+                url = "https://travel-shop.herokuapp.com/"
+            }else{
+                url="http://localhost:5000/"
+            }
             props.detail.images.map(item=>{
                 images.push({
-                    original: `http://localhost:5000/${item}`,
-                    thumbnail: `http://localhost:5000/${item}`
+                    original:`${url}${item}`,
+                    thumbnail: `${url}${item}`
                 })
             })
             setImages(images);
--- client/src/components/utils/ImageSlider.js
+++ client/src/components/utils/ImageSlider.js
@@ -3,12 +3,18 @@ import {Carousel} from 'antd';
 import './utils.css';
 
 function ImageSlider(props) {
+    let url="";
+    if(process.env.NODE_ENV ==="production"){
+        url = "https://travel-shop.herokuapp.com/"
+    }else{
+        url="http://localhost:5000/"
+    }
     return (
         <div>
             <Carousel autoplay>
                { props.images.map((img, index)=>(
                    <div key={index}>
-                       <img className="image" src={`http://localhost:5000/${img}`} alt="product-img"/>
+                       <img className="image" src={`${url}${img}`} alt="product-img"/>
                    </div>
                 ))}
             </Carousel>
